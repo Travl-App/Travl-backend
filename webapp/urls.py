cf319e36790a6e90517c0376fae612a040055e00
Dmitry <dmitry@unipat.ru>
2019-03-29 00:57:33 +0300

    Frontend dated from 2019-03-28 + url dispatcher /articles/<pk>

--- webapp/urls.py
+++ webapp/urls.py
@@ -9,7 +9,7 @@ app_name = "web"
 
 urlpatterns = [
     path('', index_view, name='create'),  # general page
-    # path('<int:pk>/', PlaceDetailView.as_view(), name='detail'),  # read
+    path('articles/<int:pk>/', index_view, name='article'),  # read
     # path('', PlaceListView.as_view(), name='list'),  # read-all (list)
     # path('<int:pk>/update', PlaceUpdateView.as_view(), name='update'),  # update
     # path('<int:pk>/delete', PlaceDeleteView.as_view(), name='delete'),  # delete
