ad311ebe71dac8c78e918c562ce81545de64ec3b
Dmitry <dmitry@unipat.ru>
2019-03-29 01:24:41 +0300

    url dispatcher points everything to web

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 
 from webapp.views import index_view
@@ -8,8 +8,8 @@ app_name = "web"
 
 
 urlpatterns = [
-    path('', index_view, name='create'),  # general page
-    path('articles/<int:pk>/', index_view, name='article'),  # read
+    # path('', index_view, name='create'),  # general page
+    re_path(r'^(?!api).*$', index_view, name='article'),  # read
     # path('', PlaceListView.as_view(), name='list'),  # read-all (list)
     # path('<int:pk>/update', PlaceUpdateView.as_view(), name='update'),  # update
     # path('<int:pk>/delete', PlaceDeleteView.as_view(), name='delete'),  # delete
