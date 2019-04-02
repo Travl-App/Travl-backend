3bd41ce989ae75a3e6823eb7d311bed63d4703b9
Dmitry <dmitry@unipat.ru>
2019-04-02 22:52:31 +0300

    Added article url dispatcher

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, re_path
 
 
-from webapp.views import index_view
-
+from webapp.views import index_view, article_view
 
 app_name = "web"
 
 
 urlpatterns = [
     # path('', index_view, name='create'),  # general page
-    re_path(r'^(?!api).*$', index_view, name='article'),  # read
+    # re_path(r'^(?!api).*$', index_view, name='article'),  # read
+    path('articles/<int:key>', article_view, name='detail')
     # path('', PlaceListView.as_view(), name='list'),  # read-all (list)
     # path('<int:pk>/update', PlaceUpdateView.as_view(), name='update'),  # update
     # path('<int:pk>/delete', PlaceDeleteView.as_view(), name='delete'),  # delete
--- webapp/views.py
+++ webapp/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'webapp/index.html', context={})
+
+
+def article_view(request, key):
+    assert key
+    return render(request, 'webapp/index.html', context={})
